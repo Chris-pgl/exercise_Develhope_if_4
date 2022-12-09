985c06d49ab3b5427520990349f6076bdd893dda
Chris <cr1s@hotmail.it>
2022-12-09 18:59:54 +0100

    Fix: Fixed age!

--- src/Person.java
+++ src/Person.java
@@ -8,22 +8,22 @@ public class Person {
         int minNumber = 5;
 
         //create a variable with random number
-        int age = (int) Math.floor(Math.random()*(maxNumber-minNumber+1)+minNumber);
+        this.age =  (int) Math.floor(Math.random()*(maxNumber-minNumber+1)+minNumber);
 
     }
 
     public void getLifeStage(){
         System.out.println(" The person ");
         if(age <= 12){ //1 condition
-            System.out.println(" is a Child");
+            System.out.println(" is a Child ");
         }else if(age == 13 && age < 19){  //second condition with ternary operator
             System.out.println(" is a Teen ");
         }else if(age == 20 && age < 59){
-            System.out.println(" is an Adult");
+            System.out.println(" is an Adult ");
         }else{
-            System.out.println(" is a Senior Adult");
+            System.out.println(" is a Senior Adult ");
         }
-        System.out.println(" stage of life");
+        System.out.println(" stage of life "+ age);
     }
 
 }
